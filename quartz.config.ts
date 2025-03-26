83f5879ae857bfc22a998bc382cc28109b40b42f
highants <highants@gmail.com>
2025-03-26 22:02:41 +0900

    ok

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
+        header: "NotoSerifJP-Regular",
         body: "NotoSerifJP-Regular",
         code: "IBM Plex Mono",
       },
