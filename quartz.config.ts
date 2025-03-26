fcf7a4b3ac04f0d40d073913330414438424126d
highants <highants@gmail.com>
2025-03-26 20:08:36 +0900

    vault backup: 2025-03-26 20:08:35

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
-        body: "Noto Sans Japanese",
+        body: "Noto Sans JP",
         code: "IBM Plex Mono",
       },
       colors: {
