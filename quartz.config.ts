7d22fd1b0ab2fd226b751d104114fb9c93d38c16
highants <highants@gmail.com>
2025-04-09 20:00:04 +0900

    baseurl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "ja-JP",
-    baseUrl: "highants.github.io/quartz",
+    baseUrl: "highants.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
