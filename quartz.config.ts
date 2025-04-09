1bbfec80448005a44ab14bd299d5924dc09924cd
highants <highants@gmail.com>
2025-04-09 21:03:48 +0900

    titlesuffix

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "🏖️🌴",
-    pageTitleSuffix: "",
+    pageTitleSuffix: "🏖️🌴",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
